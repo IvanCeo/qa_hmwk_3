35a874ee448ca9fbeed1e62b9400ddfa77e41184
Ivan <ceo@ivan-victorovich.ru>
2025-05-13 11:14:18 +0300

    2nd

--- test_booking_service_Mikhailov_Ivan_Victorovich.py
+++ test_booking_service_Mikhailov_Ivan_Victorovich.py
@@ -1,9 +1,38 @@
 import pytest
 
-def calc_price(base_price: float, discount: float, count: int) -> float:
-    pass
+class Event():
+    def __init__(self, id, base_price: float, avaliable_tickets: int, booked_tickets: int):
+        self.id = id
+        self.base_price = base_price
+        self.avaliable_tickets = avaliable_tickets
+        self.booked_tickets = booked_tickets
 
-def test_calc_price():
+
+def calc_price(base_price: float, discount: float, count: int) -> float:
+    if (
+        count == 0 or
+        discount < 0 or
+        discount > base_price or
+        base_price < 0 or
+        isinstance(count, int) or
+        isinstance(base_price, float) or
+        isinstance(discount, float)
+    ):
+        raise ValueError
+    else:
+        return (base_price - discount)* count
+
+
+@pytest.mark.parametrize(
+        "base_price, discount, count",
+        [
+            (0.0, 0.1, 2),
+            (12, 1, -3),
+        ]
+)
+def test_calc_price(base_price, discount, count):
     with pytest.raises(ValueError):
-        calc_price(base_price=0.0, discount=0.1, count=2)
+        calc_price(base_price, discount, count)
+    
+
 
